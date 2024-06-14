09f001e2e35729d7888d19dce74eaf41a354e28d
SerSul <d.nikola1704@gmail.com>
2024-06-14 17:32:24 +0300

    implemented correct conversion from Utf8 to Wstring

--- privilege_check.cpp
+++ privilege_check.cpp
@@ -5,36 +5,48 @@
 
 #pragma comment(lib, "netapi32.lib")
 
+/*
+
+  USER_PRIV_GUEST     0
+  USER_PRIV_USER      1
+  USER_PRIV_ADMIN     2
+
+  NetUserGetInfo (
+        LPCWSTR    servername OPTIONAL,
+        LPCWSTR    username,
+        DWORD      level,
+        LPBYTE     *bufptr
+  );
+
+ */
+
+#define PRIVILEGE_STRINGS {"Пользователь", "Гость", "Администратор"}
+#define NET_USER_LEVEL 1
+#define NET_API_SUCCESS NERR_Success
+#define NET_API_ERROR "нет"
+
+std::wstring Utf8ToWstring(const std::string& str) {
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.data(), str.size(), nullptr, 0);
+    std::wstring wstr(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.data(), str.size(), &wstr[0], size_needed);
+    return wstr;
+}
+
 Napi::String CheckUserPrivilege(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
+    std::wstring wname = Utf8ToWstring(info[0].As<Napi::String>());
 
-    if (info.Length() < 1 || !info[0].IsString()) {
-        Napi::TypeError::New(env, "Expected a string argument").ThrowAsJavaScriptException();
-        return Napi::String::New(env, "Error");
-    }
-
-    std::string name = info[0].As<Napi::String>().Utf8Value();
-
-    std::wstring wname(name.begin(), name.end());
-    LPWSTR username = (LPWSTR)wname.c_str();
-
-    DWORD level = 1;
-    LPBYTE buf = nullptr;
-    NET_API_STATUS res = NetUserGetInfo(NULL, username, level, &buf);
-    if (res == NERR_Success) {
-        USER_INFO_1* userInfo = (USER_INFO_1*)buf;
-        std::string privilege;
-        if (userInfo->usri1_priv == USER_PRIV_ADMIN) {
-            privilege = "Администратор";
-        } else if (userInfo->usri1_priv == USER_PRIV_GUEST) {
-            privilege = "Гость";
-        } else {
-            privilege = "Пользователь";
-        }
-        NetApiBufferFree(buf); 
+    DWORD level = NET_USER_LEVEL;
+    LPBYTE buffer = nullptr;
+    NET_API_STATUS res = NetUserGetInfo(nullptr, wname.c_str(), level, &buffer);
+    if (res == NET_API_SUCCESS) {
+        USER_INFO_1* userInfo = reinterpret_cast<USER_INFO_1*>(buffer);
+        const char* privilegeStrings[] = PRIVILEGE_STRINGS;
+        std::string privilege = privilegeStrings[userInfo->usri1_priv];
+        NetApiBufferFree(buffer);
         return Napi::String::New(env, privilege);
     } else {
-        return Napi::String::New(env, "нет");
+        return Napi::String::New(env, NET_API_ERROR);
     }
 }
 
